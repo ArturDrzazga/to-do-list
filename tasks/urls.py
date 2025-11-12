24e4ed8f90e4ac41cefb7ddb9ba82e0d92dd8ed1
Artur Drzazga <artur.drzazga03@gmail.com>
2025-11-12 18:57:35 +0100

    added toggle sign for tasks list

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 
-from tasks.views import TasksListView, TasksCreateView, TagsListView, TagsCreateView, TasksUpdateView, TasksDeleteView, \
-    TagsUpdateView, TagsDeleteView
+from tasks.views import (TasksListView,
+                         TasksCreateView,
+                         TagsListView,
+                         TagsCreateView,
+                         TasksUpdateView,
+                         TasksDeleteView,
+                         TagsUpdateView,
+                         TagsDeleteView, ToggleTaskView)
 
 urlpatterns = [
     path("", TasksListView.as_view(), name="tasks-list"),
     path("create/", TasksCreateView.as_view(), name="tasks-create"),
     path("update/<int:pk>/", TasksUpdateView.as_view(), name="tasks-update"),
     path("delete/<int:pk>/", TasksDeleteView.as_view(), name="tasks-delete"),
+    path("<int:pk>/toggle-task/", ToggleTaskView.as_view(), name="toggle-task"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
     path("tags/create/", TagsCreateView.as_view(), name="tags-create"),
     path("tags/update/<int:pk>/", TagsUpdateView.as_view(), name="tags-update"),
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 
 from .forms import TaskForm
 from .models import Task, Tag
@@ -27,6 +28,14 @@ class TasksDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("tasks:tasks-list")
 
+
+class ToggleTaskView(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+        task.save()
+
+        return redirect("tasks:tasks-list")
 
 class TagsListView(generic.ListView):
     model = Tag
